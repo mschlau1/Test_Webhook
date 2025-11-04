0fa8ece8a57918bd93dcc0d3b93ab19823c9ba5a
Matthew Schlau <mschlau@us.ibm.com>
2025-11-03 19:48:17 -0600

    Adding DockerFile

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+// Tell Express to trust the X-Forwarded-* headers from proxies
+app.set('trust proxy', true);
+
 app.get('/', (req, res) => {
-  // Get client IP address
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  res.send(`Your IP address is: ${ip}`);
+  // Express will automatically parse X-Forwarded-For when 'trust proxy' is true
+  const ip = req.ip;
+  const allIps = req.ips; // if multiple proxies are in the chain
+  res.send({
+    originating_ip: ip,
+    forwarded_chain: allIps
+  });
 });
 
 const PORT = 3000;
